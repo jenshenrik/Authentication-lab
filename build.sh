c13fa71906c1a40ee4044e1ed1b6e67585c768b7
Jens Henrik Skuldbøl <jenshenrik.skuldbol@gmail.com>
2013-11-18 01:12:50 +0100

    report writings

---
+++ build.sh
@@ -0,0 +1,1 @@
+javac -d bin/ src/sample/*.java src/sample/module/*.java src/sample/principal/*.java
--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-#!/usr/bin/bash
-
 java -classpath bin -Djava.security.auth.login.config==sample_jaas.config sample.SampleAcn
